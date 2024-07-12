e62613c47b8fdf87abd3168e9f4392935b928e0e
Vinay Bhupelli <vinaybhupelli1@gmail.com>
2024-07-12 20:06:52 +0530

    changed environment name

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "netflixgpt-57508.firebaseapp.com",
   projectId: "netflixgpt-57508",
   storageBucket: "netflixgpt-57508.appspot.com",
