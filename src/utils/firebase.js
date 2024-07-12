d10447cfa42f7fe182942bcbe7c591f6abaeceb8
Vinay Bhupelli <vinaybhupelli1@gmail.com>
2024-07-12 19:10:54 +0530

    Firebase API key removed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAmynfwvW6dM2eRnVll7gqtvf9fNo132ts",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "netflixgpt-57508.firebaseapp.com",
   projectId: "netflixgpt-57508",
   storageBucket: "netflixgpt-57508.appspot.com",
